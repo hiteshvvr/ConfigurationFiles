f0ea6f3996ed94cb6b51bda0b03d062606f4a335
Hitesh <37035616+hiteshvvr@users.noreply.github.com>
2020-02-07 17:57:00 +0200

    created installpythonpackages.sh

---
+++ installpythonpackages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## declare an array variable
+declare -a arr=("element1" "element2" "element3")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   echo "$i"
+   # or do whatever with individual element of the array
+done
+
+
+
+
+["glob" "warnings" "os" "time" "multiprocessing" "plotly" "hvplot" "pprint" "peakutils" "pandas" "numpy" "lmfit" "pybroom" "uncertainties " "holoviews" "sty" "uproot" "abel" "colorlover" "pathlib " ]
