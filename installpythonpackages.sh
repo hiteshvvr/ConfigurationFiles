3f0a6d344dd3ef05f36dbf6adacdd272a816afda
hiteshvvr <hitesh.vvr@gmail.com>
2020-03-03 09:58:34 +0200

    Added the pip install library file

--- installpythonpackages.sh
+++ installpythonpackages.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 ## declare an array variable
-declare -a arr=("element1" "element2" "element3")
+declare -a tarr=("element1" "element2" "element3")
+declare -a arr=("glob" "warnings" "os" "time" "multiprocessing" "plotly" "hvplot" "pprint" "peakutils" "pandas" "numpy" "lmfit" "pybroom" "uncertainties " "holoviews" "sty" "uproot" "abel" "colorlover" "pathlib ")
 
 ## now loop through the above array
 for i in "${arr[@]}"
 do
-   echo "$i"
+   echo " Installing ----> $i"
+   pip install $i
    # or do whatever with individual element of the array
 done
 
 
 
 
-["glob" "warnings" "os" "time" "multiprocessing" "plotly" "hvplot" "pprint" "peakutils" "pandas" "numpy" "lmfit" "pybroom" "uncertainties " "holoviews" "sty" "uproot" "abel" "colorlover" "pathlib " ]
