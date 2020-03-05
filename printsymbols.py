6e412ed099932483f9e86889e45c304c5a07b102
Hitesh <hitesh.vvr@gmail.com>
2020-03-05 12:23:00 +0200

    added python code to print utf-8 encoded symbols

---
+++ printsymbols.py
@@ -0,0 +1,31 @@
+
+print("Below are the usual symbols")
+print("Om1                  :   [2768]  \t\t", chr(2768))
+print("Om2                  :   [2384]  \t\t", chr(2384))
+print("Om3                  :   [128329]\t\t", chr(128329))
+print("Tamil Om             :   [3024]  \t\t", chr(3024))
+print("Tibet Om             :   [3840]  \t\t", chr(3840))
+print("Jain  Om             :   [2384]  \t\t", chr(2384))
+print("dots1                :   [9898]  \t\t", chr(9898))
+print("dots2                :   [9899]  \t\t", chr(9899))
+print("dots3                :   [9675]  \t\t", chr(9675))
+print("dots4                :   [9679]  \t\t", chr(9679))
+print("Nuclear              :   [9885]  \t\t", chr(9885))
+print("Ancher               :   [9875]  \t\t", chr(9875))
+print("Clock                :   [9202]  \t\t", chr(9202))
+
+print("you can also run the for loops by modifying the code")
+for j in range(20):
+  print(" ")
+  print("from", j*1000 , "to", j*1000+1000)
+  for i in range(1000):
+    m = 1000*j+i
+    print(chr(m), end = " ")
+
+
+# for i in range(2000,2500,1):
+#   print("[",i,": ",chr(i),"]")
+
+# print("Om: ", chr())¬
+# print("OmTamil: ", chr())¬
+# print("Om Tibet: ", chr(3840))¬
