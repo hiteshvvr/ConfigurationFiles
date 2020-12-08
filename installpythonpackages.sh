241f7a436451e00bccfda79d9b5029aa3c1f4085
Hitesh <hitesh.vvr@gmail.com>
2020-12-08 12:21:12 +0200

    added line be line packages for windows

--- installpythonpackages.sh
+++ installpythonpackages.sh
@@ -11,6 +11,25 @@ do
    # or do whatever with individual element of the array
 done
 
-
+# pip install glob
+# pip install warnings
+# pip install os
+# pip install time
+# pip install multiprocessing
+# pip install plotly
+# pip install hvplot
+# pip install pprint
+# pip install peakutils
+# pip install pandas
+# pip install numpy
+# pip install lmfit
+# pip install pybroom
+# pip install uncertainties 
+# pip install holoviews
+# pip install sty
+# pip install uproot
+# pip install abel
+# pip install colorlover
+# pip install pathlib 
 
 
