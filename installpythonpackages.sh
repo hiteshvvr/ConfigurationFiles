7b99b5944ec108138b1a69e6a03d8bebd2b96b1b
Hitesh <hitesh.vvr@gmail.com>
2021-01-02 22:26:06 +0200

    added for windows

--- installpythonpackages.sh
+++ installpythonpackages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## declare an array variable
 declare -a tarr=("element1" "element2" "element3")
-declare -a arr=("glob" "warnings" "os" "time" "multiprocessing" "plotly" "hvplot" "pprint" "peakutils" "pandas" "numpy" "lmfit" "pybroom" "uncertainties " "holoviews" "sty" "uproot" "abel" "colorlover" "pathlib ")
+declare -a arr=("glob" "warnings" "os" "time" "multiprocessing" "plotly" "hvplot" "pprint" "peakutils" "pandas" "numpy" "lmfit" "pybroom" "uncertainties " "holoviews" "sty" "uproot" "abel" "colorlover" "pathlib " "opencv-python" "PyQt5" "pyqtgraph")
 
 ## now loop through the above array
 for i in "${arr[@]}"
