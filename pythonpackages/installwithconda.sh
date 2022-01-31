b77499d0e764955f3213579ed42a1bbdc3bc0d88
Hitesh Rahangdale <hitesh.vvr@gmail.com>
2022-01-31 18:40:34 -0500

    installwithcondaadded

---
+++ pythonpackages/installwithconda.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+conda install -c conda-forge hvplot plotly peakutils pandas numpy lmfit
+
+# conda install -c conda-forge plotly 
+# conda install -c conda-forge hvplot
+# conda install -c conda-forge peakutils
+# conda install -c conda-forge pandas
+# conda install -c conda-forge numpy
+# conda install -c conda-forge lmfit
+conda install -c conda-forge pybroom
+conda install -c conda-forge uncertainties 
+conda install -c conda-forge holoviews
+conda install -c conda-forge sty
+conda install -c conda-forge uproot
+conda install -c conda-forge abel
+conda install -c conda-forge colorlover
+conda install -c conda-forge opencv-python
+conda install -c conda-forge PyQt5
+conda install -c conda-forge pyqtgraph
+conda install -c conda-forge jupyter_contrib_nbextensions
