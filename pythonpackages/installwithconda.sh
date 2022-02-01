6822e711eed1211a33051d5c84a838d93a2a188f
Hitesh Rahangdale <hitesh.vvr@gmail.com>
2022-01-31 19:29:44 -0500

    added condainstall things

--- pythonpackages/installwithconda.sh
+++ pythonpackages/installwithconda.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
 
-conda install -c conda-forge hvplot plotly peakutils pandas numpy lmfit
+conda install -c conda-forge hvplot plotly peakutils pandas numpy lmfit pybroom uncertainties
+conda install -c conda-forge holoviews uproot pyabel sty colorlover opencv pyqt pyqtgraph
+conda install -c conda-forge jupyter_contrib_nbextensions
 
-# conda install -c conda-forge plotly 
+pip install opencv-python
+pip install uproot
+
 # conda install -c conda-forge hvplot
+# conda install -c conda-forge plotly 
 # conda install -c conda-forge peakutils
 # conda install -c conda-forge pandas
 # conda install -c conda-forge numpy
 # conda install -c conda-forge lmfit
-conda install -c conda-forge pybroom
-conda install -c conda-forge uncertainties 
-conda install -c conda-forge holoviews
-conda install -c conda-forge sty
-conda install -c conda-forge uproot
-conda install -c conda-forge abel
-conda install -c conda-forge colorlover
-conda install -c conda-forge opencv-python
-conda install -c conda-forge PyQt5
-conda install -c conda-forge pyqtgraph
-conda install -c conda-forge jupyter_contrib_nbextensions
+# conda install -c conda-forge pybroom
+# conda install -c conda-forge uncertainties 
+# conda install -c conda-forge holoviews
+# conda install -c conda-forge uproot
+# conda install -c conda-forge pyabel
+# conda install -c conda-forge sty
+# conda install -c conda-forge colorlover
+# conda install -c conda-forge opencv
+# conda install -c conda-forge pyqt
+# conda install -c conda-forge pyqtgraph
+# conda install -c conda-forge jupyter_contrib_nbextensions
+
+
+
+
